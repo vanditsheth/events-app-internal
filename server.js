c35af5ec23725a778e961f9775acccf3a272fab9
your_github_user_name <your_email_on_github>
2021-11-11 09:52:18 +0000

    Added changes

--- server.js
+++ server.js
@@ -28,7 +28,9 @@ const mockEvents = {
     events: [
         { title: 'an event', id: 1, description: 'something really cool' },
         { title: 'another event', id: 2, description: 'something even cooler' },
-        { title: 'another another event', id: 3, description: 'not quite cool' }   ]
+        { title: 'another another event', id: 3, description: 'not quite cool' },
+        { title: '4th event', id: 4, description: 'amazing' }
+    ]
 };
 
 // health endpoint - returns an empty array
